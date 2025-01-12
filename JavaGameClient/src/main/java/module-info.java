838d903b3b2a9738de280a3d9ea74de8c4782e75
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-01-12 15:24:34 +0300

    Added ids and controller

--- JavaGameClient/src/main/java/module-info.java
+++ JavaGameClient/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.javagameclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.javagameclient to javafx.fxml;
     exports com.mycompany.javagameclient;
