059a7e77eea191da6678d8e429b2e1675d4a3e50
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-12 23:25:59 +0200

    create level Screen & controllers

---
+++ JavaGameClient/src/main/java/com/mycompany/javagameclient/LevelScreenController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javagameclient;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class LevelScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void onEasyClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHardClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMediumClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
