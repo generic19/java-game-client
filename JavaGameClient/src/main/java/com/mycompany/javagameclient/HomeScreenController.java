5c8315a4bead6ce986aa5c6cde7e619b0bb29d26
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-12 13:18:18 +0200

    add background images & add Homecontroller

---
+++ JavaGameClient/src/main/java/com/mycompany/javagameclient/HomeScreenController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javagameclient;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class HomeScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void onLoginClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onPlayOnlineClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPlayWithDriend(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSignUpClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onplayWithComputer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
