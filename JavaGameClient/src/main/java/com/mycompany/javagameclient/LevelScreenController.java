10ef2a3a771b5d1af6591fb5fea94363667cbb16
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-01-14 22:31:43 +0200

    Added functionality to levels buttons

--- JavaGameClient/src/main/java/com/mycompany/javagameclient/LevelScreenController.java
+++ JavaGameClient/src/main/java/com/mycompany/javagameclient/LevelScreenController.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.javagameclient;
-import com.mycompany.game.GameAgent;
+import com.mycompany.game.*;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,18 +13,18 @@ import javafx.scene.Parent;
 public class LevelScreenController {
 
     @FXML
-    void onEasyClicked(ActionEvent event) {
-
+    void onEasyClicked(ActionEvent event) throws IOException {
+        startGameWithAgent(new EasyAgent());
     }
 
     @FXML
-    void onHardClicked(ActionEvent event) {
-
+    void onHardClicked(ActionEvent event) throws IOException {
+        startGameWithAgent(new HardAgent());
     }
 
     @FXML
-    void onMediumClicked(ActionEvent event) {
-
+    void onMediumClicked(ActionEvent event) throws IOException {
+        startGameWithAgent(new MediumAgent());
     }
     
     private void startGameWithAgent(GameAgent agent) throws IOException {
@@ -34,5 +34,6 @@ public class LevelScreenController {
         XoGameController controller = loader.getController();
         controller.initializeGameForLocalWithComputer(agent);
         
+        App.setRoot(root);
     }
 }
