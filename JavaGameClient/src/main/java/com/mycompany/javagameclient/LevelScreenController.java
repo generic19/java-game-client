15836007dd57885adedf2bfd1e6aae8b98c8935d
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-24 23:25:09 +0200

    edit levelscreen to be responsive & change fonts in some screens

--- JavaGameClient/src/main/java/com/mycompany/javagameclient/LevelScreenController.java
+++ JavaGameClient/src/main/java/com/mycompany/javagameclient/LevelScreenController.java
@@ -1,16 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ ///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+
+
 package com.mycompany.javagameclient;
-import com.mycompany.game.*;
+
+import com.mycompany.game.GameAgent;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+import com.mycompany.game.*;
+import javafx.beans.binding.DoubleBinding;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
+
 
 public class LevelScreenController {
+
+    @FXML
+    private StackPane levelStack;
+
+    @FXML
+    private VBox innerBox;
+
+    @FXML
+    void onBackClicked(ActionEvent event) {
+            App.switchToFXML("HomeScreen");  
+    }
 
     @FXML
     void onEasyClicked(ActionEvent event) throws IOException {
@@ -26,19 +51,34 @@ public class LevelScreenController {
     void onMediumClicked(ActionEvent event) throws IOException {
         startGameWithAgent(new MeduimAgent());
     }
-    
+
+    @FXML
+    void initialize() {
+        levelStack.setBackground(new Background(
+                new BackgroundImage(
+                        new Image(App.class.getResource("images/GameBackGround.png").toExternalForm()),
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.CENTER,
+                        new BackgroundSize(1, 1, true, true, false, true)
+                )
+        ));
+
+        DoubleBinding scaleFactor = levelStack.heightProperty().divide(1000).add(0.65);
+
+        innerBox.scaleXProperty().bind(scaleFactor);
+        innerBox.scaleYProperty().bind(scaleFactor);
+
+    }
+
     private void startGameWithAgent(GameAgent agent) throws IOException {
         FXMLLoader loader = App.getFXMLLoader("xoGame");
         Parent root = loader.load();
-        
+
         XoGameController controller = loader.getController();
         controller.initializeGameForLocalWithComputer(agent);
         
-        App.setRoot(root);
-    }
 
-    @FXML
-    private void onBackClicked(ActionEvent event) {
-        App.switchToFXML("HomeScreen");
+        App.setRoot(root);
     }
 }
